7644ef3ed9c22d147f624b0061f8762bd65c58d0
Phantom Knight <PhantomKnight-bot@users.noreply.github.com>
2023-02-18 16:29:10 +0530

    Release

---
+++ chunks/about-us.php
@@ -0,0 +1,7 @@
+<section class="fabout">
+		<div class="section white center">
+		      <div class="row container">
+		        <h2 class="header">ABOUT US</h2>
+		        <p class="grey-text text-darken-3 lighten-3">F1 Drive In in Kalaburagi Hcb, Gulbarga is a top player in the category Middle East Restaurants in the Gulbarga. This well-known establishment acts as a one-stop destination servicing customers both local and from other parts of Gulbarga. Over the course of its journey, this business has established a firm foothold in it’s industry. The belief that customer satisfaction is as important as their products and services, have helped this establishment garner a vast base of customers, which continues to grow by the day. This business employs individuals that are dedicated towards their respective roles and put in a lot of effort to achieve the common vision and larger goals of the company. In the near future, this business aims to expand its line of products and services and cater to a larger client base. In Gulbarga, this establishment occupies a prominent location in Kalaburagi Hcb. It is an effortless task in commuting to this establishment as there are various modes of transport readily available. It is known to provide top service in the following categories: Restaurants, Middle East Restaurants, Drive In Restaurants.</p>
+		      </div>
+	</section>
---
+++ chunks/description.php
@@ -0,0 +1,13 @@
+<section class="fdes">
+		<div class="section white center">
+			<h2 class="header" style="padding:20px; padding-bottom: 30px;">Resturant Powered By Arabian Nights</h2>
+		      <div class="row container center">
+		        <div class="col center l8 s12">
+		        	<p>F1 Drive In in Kalaburagi Hcb, Gulbarga is a top player in the category Middle East Restaurants in the Gulbarga. This well-known establishment acts as a one-stop destination servicing customers both local and from other parts of Gulbarga. Over the course of its journey, this business has established a firm foothold in it’s industry. The belief that customer satisfaction is as important as their products and services, have helped this establishment garner a vast base of customers, which continues to grow by the day. This business employs individuals that are dedicated towards their respective roles and put in a lot of effort to achieve the common vision and larger goals of the company. In the near future, this business aims to expand its line of products and services and cater to a larger client base. In Gulbarga, this establishment occupies a prominent location in Kalaburagi Hcb. It is an effortless task in commuting to this establishment as there are various modes of transport readily available. It is known to provide top service in the following categories: Restaurants, Middle East Restaurants, Drive In Restaurants.</p>
+		        </div>
+		        <div class="col center l4 s12">
+		        	<img height="300" width="auto" style="object-fit: contain;" src="images/logo.jpg" alt="">
+		        </div>
+		        
+		      </div>
+	</section>
